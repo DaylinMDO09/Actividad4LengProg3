0cecf649c237bc4a16189fbabf86c14c170bd8a8
DaylinMDO09 <daylinmf09@gmail.com>
2025-06-20 21:04:34 -0400

    Initial commit

---
+++ Controllers/EstudiantesController.cs
@@ -0,0 +1,97 @@
+﻿using Actividad3LengProg3.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace Actividad3LengProg3.Controllers
+{
+    public class EstudiantesController : Controller
+    {
+        private static List<EstudianteViewModel> estudiantes = new List<EstudianteViewModel>();
+
+        
+        public IActionResult Index()
+        {
+            return View(new EstudianteViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Registrar(EstudianteViewModel model) 
+        {
+            if (ModelState.IsValid)
+            {
+                estudiantes.Add(model);
+                TempData["Mensaje"] = "Estudiante registrado correctamente.";
+            }
+
+            return View("Index", model);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(EstudianteViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                EstudianteViewModel estudianteActual = estudiantes.FirstOrDefault(e => e.matriculaEstudiante.Equals(model.matriculaEstudiante));
+
+                if (estudianteActual == null)
+                {
+                    TempData["MensajeError"] = "No existe el estudiante indicado.";
+
+                    return RedirectToAction("Lista");
+                }
+
+                if (estudianteActual != null)
+                {
+                    estudianteActual.matriculaEstudiante = model.matriculaEstudiante;
+                    estudianteActual.nombreEstudiante = model.nombreEstudiante;
+                    estudianteActual.carreraEstudiante = model.carreraEstudiante;
+                    estudianteActual.correoEstudiante = model.correoEstudiante;
+                    estudianteActual.telefonoEstudiante = model.telefonoEstudiante;
+                    estudianteActual.fechaEstudiante = model.fechaEstudiante;
+                    estudianteActual.generoEstudiante = model.generoEstudiante;
+                    estudianteActual.turnoEstudiante = model.turnoEstudiante.ToString();
+                    estudianteActual.ingresoEstudiante = model.ingresoEstudiante;
+                    estudianteActual.becaEstudiante = model.becaEstudiante;
+                    estudianteActual.porcentajebecaEstudiante = model.porcentajebecaEstudiante;
+
+                    TempData["Mensaje"] = "Los datos del estudiante han sido editados satisfactoriamente.";
+                    return RedirectToAction("Lista");
+                }
+            }
+
+           return View(model); 
+        }
+
+        public IActionResult Eliminar (string matricula) 
+        {
+            var estudiante = estudiantes.FirstOrDefault(e => e.matriculaEstudiante == matricula);
+            if (estudiante != null) estudiantes.Remove(estudiante);
+            TempData["Mensaje"] = "Estudiante eliminado satisfactoriamente.";
+            return RedirectToAction("Lista");
+        }
+
+        [HttpGet]
+        public IActionResult Lista()
+        {
+            return View(estudiantes);
+        }
+
+        [HttpGet]
+        public IActionResult Editar(string matricula) 
+        {
+            if (string.IsNullOrEmpty(matricula))
+            {
+                TempData["MensajeError"] = "La matricula de este estudiante no existe.";
+                return RedirectToAction("Lista");
+            }
+
+            EstudianteViewModel estudianteActual = estudiantes.FirstOrDefault(e => e.matriculaEstudiante.Equals(matricula));
+
+            if (estudianteActual == null)
+            {
+                TempData["MensajeError"] = "El estudiante indicado no existe.";
+                return RedirectToAction("Lista");
+            }
+
+            return View(estudianteActual);
+        }
+    }
+}
---
+++ Models/EstudianteViewModel.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+namespace Actividad3LengProg3.Models
+{
+    public class EstudianteViewModel
+    {
+        [Required(ErrorMessage = "El nombre del estudiante es necesario")]
+        [StringLength(100)]
+        [Display(Name = "Nombre completo")]
+        public string nombreEstudiante { get; set; }
+
+        [Required(ErrorMessage = "La matricula es obligatória")]
+        [StringLength(20, MinimumLength = 6)]
+        [Display(Name = "Matricula")]
+        public string matriculaEstudiante { get; set; }
+
+        [Required(ErrorMessage = "Debes de elegir una carrera")]
+        [Display (Name = "Carrera")]
+        public string carreraEstudiante { get; set; }
+        public List<SelectListItem> carreras { get; } = new List<SelectListItem>
+        {
+            new SelectListItem {Value = "Ingeniería en software", Text = "Ingeniería en software"},
+            new SelectListItem {Value = "Odontología", Text = "Odontología"},
+            new SelectListItem {Value = "Administración de empresas", Text = "Administración de empresas"},
+            new SelectListItem {Value = "Enfermería", Text = "Enfermería"},
+            new SelectListItem {Value = "Psicología", Text = "Psicología"}
+        };
+
+        [Required (ErrorMessage = "El correo es obligatorio")]
+        [EmailAddress (ErrorMessage = "Correo inválido")]
+        [Display (Name = "Correo institucional")]
+        public string correoEstudiante { get; set; }
+
+        [Phone (ErrorMessage = "El número telefónico es obligatorio")]
+        [MinLength(10)]
+        [Display(Name = "Número telefónico")]
+        public string telefonoEstudiante { get; set; }
+
+        [Required (ErrorMessage = "Debe indicar su fecha de nacimiento")]
+        [DataType (DataType.Date)]
+        [Display (Name = "Fecha de nacimiento")]
+        public DateTime? fechaEstudiante { get; set; }
+
+        [Required]
+        [Display (Name = "Género")]
+        public string generoEstudiante { get; set; }
+        
+        [Required (ErrorMessage = "Debes elegir un turno")]
+        [Display (Name = "Turnos")]
+        public string turnoEstudiante { get; set; }
+        public List<SelectListItem> turnos { get; } = new List<SelectListItem>
+        {
+            new SelectListItem {Value = "Matutino", Text = "Matutino"},
+            new SelectListItem {Value = "Vespertino", Text = "Vespertino"},
+            new SelectListItem {Value = "Nocturno", Text = "Nocturno"}
+        };
+
+        [Required (ErrorMessage = "Elige tu tipo de ingreso")]
+        [Display (Name = "Tipo de ingreso")]
+        public string ingresoEstudiante {get; set; }
+        public List<SelectListItem> ingresos { get; } = new List<SelectListItem>
+        {
+            new SelectListItem {Value = "Nuevo", Text = "Nuevo ingreso"},
+            new SelectListItem {Value = "Reingreso", Text = "Reingreso"},
+            new SelectListItem {Value = "Transfer", Text = "Ingreso por transferencia"},
+            new SelectListItem {Value = "DóM", Text = "Ingreso por Doctorado o Maestría"}
+        };
+
+        [Display (Name ="¿Eres becado?")]
+        public bool becaEstudiante { get; set; }
+
+        [Display (Name ="Porcentaje de la beca (en caso de tener beca): ")]
+        [Range (0,100)]
+        public int? porcentajebecaEstudiante { get; set; }
+
+        [Range(typeof(bool), "true", "true", ErrorMessage = "Debes aceptar los términos y condiciones de la institución.")]
+        [Display(Name = "Acepto los terminos y condiciones.")]
+        public bool tcEstudiante { get; set; }
+
+    }
+}
