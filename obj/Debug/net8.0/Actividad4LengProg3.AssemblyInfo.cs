fc17550d11bd4f6c69fcb9899bb8a2026a3e88c2
DaylinMDO09 <daylinmf09@gmail.com>
2025-07-10 20:54:59 -0400

    Actividad 4 (10/7)

--- obj/Debug/net8.0/Actividad4LengProg3.AssemblyInfo.cs
+++ obj/Debug/net8.0/Actividad4LengProg3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Actividad4LengProg3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11fc55400de07e327f2060d6817f1010ac4a86e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65d7545d99db5c5a1cdad5485cea3665a71fd057")]
 [assembly: System.Reflection.AssemblyProductAttribute("Actividad4LengProg3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Actividad4LengProg3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
